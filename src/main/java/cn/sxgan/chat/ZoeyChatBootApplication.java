4d850d25fff3562a86edbddd695213e27466ec23
sxgan <sxgan@foxmail.com>
2024-07-31 18:08:50 +0800

    【项目初始化】基础搭建

---
+++ src/main/java/cn/sxgan/chat/ZoeyChatBootApplication.java
@@ -0,0 +1,13 @@
+package cn.sxgan.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoeyChatBootApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ZoeyChatBootApplication.class, args);
+    }
+    
+}
